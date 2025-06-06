85622d55e182a3d69486e3baa3c05a06ea72f235
Mika Cohen <mjc@kernel.org>
2025-06-05 19:16:51 -0600

    Maybe fix the flake

--- flake.nix
+++ flake.nix
@@ -1,51 +1,70 @@
 {
-  description = "A devShell example";
+  description = "Rust development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
     flake-utils,
-    ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs = [
-              openssl_3
-              pkg-config
-              eza
-              fd
-              (
-                rust-bin.selectLatestNightlyWith (toolchain:
-                  toolchain.default.override {
-                    extensions = [
-                      "rust-src"
-                      "rust-analyzer"
-                    ];
-                  })
-              )
+        pkgs = nixpkgs.legacyPackages.${system};
+        # Read the file relative to the flake's root
+        overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
+        libPath = with pkgs;
+          lib.makeLibraryPath [
+            # load external libraries that you need in your rust project here
+          ];
+      in {
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = [pkgs.pkg-config];
+          buildInputs = with pkgs; [
+            clang
+            llvmPackages.bintools
+            rustup
+            openssl
+            openssl.dev
+            pkg-config
+            par2cmdline
+            xxd
+          ];
+
+          RUSTC_VERSION = overrides.toolchain.channel;
+
+          # https://github.com/rust-lang/rust-bindgen#environment-variables
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
+
+          shellHook = ''
+            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          '';
+
+          # Add precompiled library to rustc search path
+          RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') [
+            # add libraries here (e.g. pkgs.libvmi)
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+
+          # Add glibc, clang, glib, and other headers to bindgen search path
+          BINDGEN_EXTRA_CLANG_ARGS =
+            # Includes normal include path
+            (builtins.map (a: ''-I"${a}/include"'') [
+              # add dev libraries here (e.g. pkgs.libvmi.dev)
+              pkgs.glibc.dev
+            ])
+            # Includes with special directory paths
+            ++ [
+              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+              ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+              ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
             ];
-
-            LD_LIBRARY_PATH = lib.makeLibraryPath [openssl_3];
-
-            shellHook = ''
-              alias ls=eza
-              alias find=fd
-            '';
-          };
-        }
+        };
+      }
     );
 }
